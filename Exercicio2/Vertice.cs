f2249006c1011318ee7825148e1e2ef2bf5d439c
Isaac Farias <isaacfarias006@gmail.com>
2022-11-14 15:00:47 -0300

    Adiciona classe Vertice

---
+++ Exercicio2/Vertice.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercicio2
+{
+    public class Vertice
+    {
+        public Vertice(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; private set; }
+
+        public double Y { get; private set; }
+
+        public void Move(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static double Distancia(Vertice a, Vertice b)
+        {
+            double Square(double x) => x * x;
+            return Math.Sqrt(Square(b.X - a.X) + Square(b.Y - a.Y));
+        }
+
+        public static bool Iguais(Vertice a, Vertice b)
+        {
+            return ((a.X == b.X) && (a.Y == b.Y));
+        }
+    }
+}
