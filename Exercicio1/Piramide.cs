e8831bc6c143b68e7d8a36802c8cadb090ea6448
Isaac Farias <isaacfarias006@gmail.com>
2022-11-14 12:27:42 -0300

    Adiciona classe Piramide

--- Exercicio1/Piramide.cs
+++ Exercicio1/Piramide.cs
@@ -2,6 +2,43 @@
 {
     public class Piramide
     {
+        readonly int N;
+        public Piramide(int n)
+        {
+            if (n < 1)
+            {
+                throw new Exception("n >= 1");
+            }
+            N = n;
+        }
 
+        public void Desenha()
+        {
+            for (int linha = 1, digito; linha <= N; linha++)
+            {
+                for (int espaco = 1; espaco <= N - linha; espaco++)
+                {
+                    Console.Write(" ");
+                }
+
+                digito = 1;
+
+                while (digito <= linha)
+                {
+                    Console.Write(digito);
+                    digito++;
+                }
+
+                digito = linha - 1;
+
+                while (digito >= 1)
+                {
+                    Console.Write(digito);
+                    digito--;
+                }
+
+                Console.Write("\n");
+            }
+        }
     }
 }
