5530eb1c35f4016734c6f5ac2c35277502e0bbb5
Isaac Farias <isaacfarias006@gmail.com>
2022-11-16 22:13:44 -0300

    Adiciona exercício 6

---
+++ Exercicio6/ListaIntervalo.cs
@@ -0,0 +1,27 @@
+﻿using Exercicio5;
+
+namespace Exercicio6
+{
+    public class ListaIntervalo
+    {
+        private List<Intervalo> lista = new List<Intervalo>();
+
+        public void Add(Intervalo intervalo)
+        {
+            foreach (Intervalo i in lista)
+                if (intervalo.TemIntersecao(i))
+                    return;
+
+            lista.Add(intervalo);
+        }
+
+        public void Imprime() 
+        {
+            var ListaOrdenada = lista.OrderBy(i => i.Inicial);
+            foreach (var intevalo in ListaOrdenada)
+            {
+                Console.WriteLine($"{intevalo.Inicial} - {intevalo.Final}");
+            }
+        }
+    }
+}
